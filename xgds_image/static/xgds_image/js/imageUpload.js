01eb4ddb8cbf066a4d1012afb552fa0796022d21
Grace Lee <yeongrace.lee@gmail.com>
2015-08-21 22:52:09 +0000

    image more info view. backend integration still left

--- xgds_image/static/xgds_image/js/imageUpload.js
+++ xgds_image/static/xgds_image/js/imageUpload.js
@@ -1,6 +1,143 @@
 var $container = $('#container'); 
 
-// images table
+/**
+ * Helpers
+ */
+function createPackeryItem(title, id, width){
+	//create a div that displays this imageView
+	var elem = document.createElement('div');
+	elem.className='item ' + width + ' lockAspect';
+	elem.id= id;
+	elem.innerHTML = "<div><strong>"+ title +"</strong></div>";
+	return elem;
+}
+
+
+function appendItem(elem){
+	//append to container
+	$container.append( elem );
+	$container.packery('appended', elem);
+
+	// call make resizeable again
+	makeResizable($container);
+}
+
+
+// creates a label anda appends to form
+function createLabel(labelText, form) {
+	var label = document.createElement('label');
+	label.innerHTML = labelText;
+	form.appendChild(label);
+}
+
+// creates select box and appends to form
+function createSelectBox(options, id, form) {
+	//Create select list
+	var selectList = document.createElement("select");
+	selectList.id = id;
+
+	//Create and append the options
+	for (var i = 0; i < options.length; i++) {
+	    var option = document.createElement("option");
+	    option.value = options[i];
+	    option.text = options[i];
+	    selectList.appendChild(option);
+	}
+	
+	form.appendChild(selectList);
+	
+	var br = document.createElement('br');
+	form.appendChild(br);
+}
+
+
+// creates text input field and appends to form
+function createInput(id, form) {
+	var inputBox = document.createElement("input");
+	inputBox.id = id;
+	form.appendChild(inputBox);
+	var br = document.createElement('br');
+	form.appendChild(br);
+}
+
+function createTextarea(id, form) {
+	var textArea = document.createElement("textarea");
+	textArea.id = id
+	form.appendChild(textArea);
+	var br = document.createElement('br');
+	form.appendChild(br);
+}
+
+
+/*
+ * Views
+ */
+function imageView(imageName, imagePath) {
+	/**
+	 * If the view doesn't exist, create one. 
+	 */
+	var elem = createPackeryItem(imageName, 'item_image_view', 'w2');
+	$('<img src="'+ imagePath +'">').appendTo(elem);
+	
+	//image description
+	var descriptionDiv = document.createElement('div');
+	descriptionDiv.innerHTML = "Source: dummy source, Author: Jane Doe, Altitude: 5800ft";
+	elem.appendChild(descriptionDiv);
+	
+	//more info html link
+	var a = document.createElement('a');
+	a.href =  "javascript:moreInfoView(\""+ imageName +"\");"; 
+	a.innerHTML = 'More info'
+	elem.appendChild(a);
+	
+	//append to container
+	appendItem(elem);
+}
+
+
+// creates a select box and attaches it to the form
+function moreInfoView(imageName) {
+	var elem = createPackeryItem(imageName, 'item_image_info_view', 'w3');
+	var formLayout = document.createElement('div');
+	formLayout.className = "formLayout";
+	
+	var sourceArray = ["dummy1", "dummy2", "dummy3"];
+	createLabel("Source", formLayout);
+	createSelectBox(sourceArray, "source", formLayout);
+	
+	var locationArray = ["location1", "location2", "location3"];
+	createLabel("Location", formLayout);
+	createSelectBox(locationArray, "location", formLayout); 
+	
+	createLabel("Latitude", formLayout);
+	createInput("latitude", formLayout);
+	
+	createLabel("Longitude", formLayout);
+	createInput("longitude", formLayout);
+
+	createLabel("Altitude", formLayout);
+	createInput("altitude", formLayout);
+	
+	var authorArray = ["author1", "author2", "author3"];
+	createLabel("Author", formLayout);
+	createSelectBox(authorArray, "author", formLayout); 
+	
+	createLabel("Description", formLayout);
+	createTextarea("description", formLayout);
+	
+    var submitBtn = document.createElement("INPUT");
+    submitBtn.setAttribute("type", "submit");
+	formLayout.appendChild(submitBtn);
+    
+    
+	elem.appendChild(formLayout);
+	appendItem(elem);
+}
+
+/* 
+ * Table View
+ */
+//images table
 defaultOptions["columnDefs"] = [{ "width": "150", "targets": 0 }];
 var imageTable = $('#image_table').DataTable(defaultOptions);
 
@@ -11,42 +148,6 @@ function createLink(path) {
 	return "<a href='javascript:imageView(\"" + imgName + "\",\"" + path + "\");'>" + imgName + "</a>";
 }
 
-// call back for onclick that creates a new div with image view
-function imageView(imageName, imagePath) {
-	/**
-	 * If the view doesn't exist, create one. 
-	 */
-	//create a div that displays this imageView
-	var elem = document.createElement('div');
-	elem.className='item w_image lockAspect';
-	elem.id='item_image_view';
-	elem.innerHTML = "<strong>"+imageName+"</strong>";
-	$('<img src="'+ imagePath +'">').width(450).height(300).appendTo(elem);
-	
-	//image description
-	var descriptionDiv = document.createElement('div');
-	descriptionDiv.innerHTML = "Source: dummy source, Author: Jane Doe, Altitude: 5800ft";
-	elem.appendChild(descriptionDiv);
-	
-	//more info html link
-	var a = document.createElement('a');
-	a.href =  'javascript:moreInfo();'; // Insted of calling setAttribute 
-	a.innerHTML = 'More info' // <a>INNER_TEXT</a>
-	elem.appendChild(a);
-	
-	//append to container
-	$container.append( elem );
-	$container.packery('appended', elem);
-
-	// call make resizeable again
-	makeResizable($container);
-}
-
-// callback for creating a more info view 
-function moreInfo() {
-	alert("more info view");
-}
-
 // add rows to the table on page load.
 $(images).each(function(){
 	var imageUrl = createLink(this['imageUrl']);
@@ -54,7 +155,9 @@ $(images).each(function(){
 	                    'dummy lat', 'dummy lon', 'dummy alt']).draw();
 });
 
-// image dropzone
+/*
+ * Image drag and drop upload
+ */
 Dropzone.options.imageDropZone = {
 	// Prevents Dropzone from uploading dropped files immediately
 	autoProcessQueue : false,
