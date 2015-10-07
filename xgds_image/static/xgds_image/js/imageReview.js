a4cca237d7a2ffa4cf070df345800590d9b68b79
Tamar Cohen <tamar.e.cohen@nasa.gov>
2015-10-07 16:49:03 -0700

    started refactoring to do code sharing for image stuff

---
+++ xgds_image/static/xgds_image/js/imageReview.js
@@ -0,0 +1,197 @@
+var $container = $('#container'); 
+bindLockItemBtnCallback($container);
+
+
+/**
+ * Helpers
+ */
+function stringContains(string, substring) {
+	// checks that string contains substring
+	return string.indexOf(substring) > -1;
+}
+
+function bindDeleteBtnCallback(htmlSnippet) {
+	htmlSnippet.find(".icon-cancel-circled").bind("click", function() {
+		// remove clicked element
+		$container.packery( 'remove', event.target.parentElement.parentElement );
+		// layout remaining item elements
+		$container.packery();
+	});	
+}
+
+function bindToggleBtnCallback(htmlSnippet) {
+	var toggleView = htmlSnippet.find("#more_info_view");
+	htmlSnippet.find("#image_view").find("#more_info_button").click({view: toggleView}, function(event) {
+		event.data.view.slideToggle();
+	});
+}
+
+function bindUpdateImageInfoCallback(htmlSnippet) {
+	htmlSnippet.find("#more_info_view").find("#more_info_form").submit(function(event) {
+		event.preventDefault(); 	// avoid to execute the actual submit of the form.
+		var url = updateImageUrl; // the script where you handle the form input.
+		var postData = $("#more_info_form").serialize();
+		$.ajax({
+			url: url,
+			type: "POST",
+			data: postData, // serializes the form's elements.
+			success: function(data)
+			{
+				setSaveStatusMessage($('#message'), data);
+
+			},
+			error: function() {
+				setSaveStatusMessage($('#message'), data);
+			}
+		});
+		return false; 
+	});
+}
+
+function bindLockItemBtnCallback(htmlSnippet) {
+	htmlSnippet.find(".icon-key").bind("click", function() {
+		var key = event.target;
+		if (stringContains(key.parentElement.className, "item")) {
+			$stamp = key.parentElement;
+		} else {
+			$stamp = key.parentElement.parentElement;
+		}
+		
+		var isStamped = $stamp.getAttribute('data-isStamped');
+		if ( isStamped == "true") {
+			$container.packery( 'unstamp', $stamp );
+			$stamp.setAttribute('data-isStamped', 'false');
+			this.style.color = "silver";
+		} else {
+			$container.packery( 'stamp', $stamp );
+			$stamp.setAttribute('data-isStamped', 'true');
+			this.style.color = "grey";
+		}
+		$container.packery();
+	});
+}
+
+function getCurrentImageAndIndex(htmlSnippet) {
+	/**
+	 * Helper that finds index in imageSetsArray
+	 * of the currently displayed image in the imageView.
+	 */
+	// get the img's name.
+	var currentImgUrl = htmlSnippet.find("img").attr('src');
+	var currentImgIndex = null;
+	// find the index of the current img
+	for (var i = 0; i < imageSetsArray.length; i++) {
+		if (imageSetsArray[i]['raw_image_url'] == currentImgUrl) {
+			currentImgIndex = i;
+			break;
+		}
+	}
+	return currentImgIndex;
+}
+
+function updateImageView(htmlSnippet, index) {
+	/**
+	 * Helper that updates the the image view info.
+	 */
+	if (index != null) {
+		var imageJson = imageSetsArray[index];
+		// update image name
+		htmlSnippet.find(".image-name strong").text(imageJson['name'])
+		// update image display
+		htmlSnippet.find('img').attr('src', imageJson['raw_image_url']);
+		// update image info 
+		
+	}
+}
+
+function bindImagePrevAndNextBtnCallback(htmlSnippet) {
+	htmlSnippet.find('.prev-button').click(function(event) {
+		// set the img src
+		var index = getCurrentImageAndIndex(htmlSnippet);
+		if (index != null) {
+			if (index == 0) {
+				index = imageSetsArray.length -1;
+			} else {
+				index = index - 1;
+			}
+			updateImageView(htmlSnippet, index);
+		}
+	});
+	
+	//TODO: when an image is uploaded, make sure to add to the imageSetsArray.
+	htmlSnippet.find(".next-button").click(function(event) {
+		var index = getCurrentImageAndIndex(htmlSnippet);
+		if (index == (imageSetsArray.length - 1)) {
+			index = 0;
+		} else {
+			index = index + 1;
+		}
+		updateImageView(htmlSnippet, index);
+	});
+}
+
+/*
+ * Image View
+ */
+function constructImageView(json) {
+	var rawTemplate = $('#template-image-view').html();
+	var compiledTemplate = Handlebars.compile(rawTemplate);
+	// append additional fields to json object to pass to handlebar
+	json.imageName = json['name'];
+	json.imagePath = json['raw_image_url'];
+	json.allAuthors = allAuthors;
+	json.allCameras = allCameras;
+	// inject new fields into the precompiled template
+	var htmlString = compiledTemplate(json);
+	var newDiv = $(htmlString);
+	// call backs
+	bindUpdateImageInfoCallback(newDiv);
+	bindToggleBtnCallback(newDiv);
+	bindDeleteBtnCallback(newDiv);
+	bindLockItemBtnCallback(newDiv);
+	bindImagePrevAndNextBtnCallback(newDiv);
+	// append the div to the container.
+	var result = $container.append(newDiv);
+	$container.packery( 'appended', newDiv);
+	makeChildrenResizable($container, newDiv);
+}
+
+function setSaveStatusMessage(handler, data){
+	if (data['status'] == 'success') {
+		handler.attr('class', 'success-message');
+	} else {
+		handler.attr('class', 'error-message');
+	}
+	handler.text(data['message']);
+	setTimeout(function() { // messages fades out.
+		handler.fadeOut().empty();
+	}, 5000);
+}
+
+
+
+
+/* 
+ * Table View
+ */
+// initialize the image table with json of existing images.
+var imageTable = $('#image_table'); 
+defaultOptions["aaData"] = imageSetsArray;
+defaultOptions["aoColumns"] = [
+                               {"mRender":function(data, type, full){
+                            	   var imageName = full['name'];
+                            	   var jsonString = JSON.stringify(full);
+                                   return "<a onclick='constructImageView(" + jsonString + ")'>"+ imageName +"</a>";
+                               }},
+                               {"mData": "camera_name"},
+                               {"mData": "lat"},
+                               {"mData": "lon"},
+                               {"mData": "altitude"},
+                               {"mData": "author_name"},
+];
+
+if ( ! $.fn.DataTable.isDataTable( '#image_table' ) ) {
+	  $('#image_table').DataTable(defaultOptions);
+}
+app.vent.trigger("mapSearch:found", imageSetsArray); 
+
--- xgds_image/views.py
+++ xgds_image/views.py
@@ -49,16 +49,13 @@ def getImageUploadPage(request):
     imageSets = IMAGE_SET_MODEL.get().objects.filter(author = request.user)
     imageSets = imageSets.order_by('creation_time')
     imageSetsJson = [json.dumps(imageSet.toMapDict()) for imageSet in imageSets]
-    # options for select boxes in the more info template.
-    allAuthors = [{'author_name_index': [str(user.username), int(user.id)]} for user in User.objects.all()]
-    allCameras = [{'camera_name_index': [str(camera.name), int(camera.id)]} for camera in CAMERA_MODEL.get().objects.all()]
     # map plus image templates for now
     fullTemplateList = list(settings.XGDS_MAP_SERVER_HANDLEBARS_DIRS)
     fullTemplateList.append(settings.XGDS_IMAGE_HANDLEBARS_DIR[0])
     templates = get_handlebars_templates(fullTemplateList)
     data = {'imageSetsJson': imageSetsJson,
-            'allAuthors': allAuthors,
-            'allCameras': allCameras,
+            'allAuthors': [{'author_name_index': [str(user.username), int(user.id)]} for user in User.objects.all()],
+            'allCameras': [{'camera_name_index': [str(camera.name), int(camera.id)]} for camera in CAMERA_MODEL.get().objects.all()],
             'templates': templates,
             'app': 'xgds_map_server/js/simpleMapApp.js'}
     return render_to_response("xgds_image/imageUpload.html", data,
@@ -74,7 +71,10 @@ def getImageSearchPage(request):
     theFormSetMaker = formset_factory(theForm, extra=0)
     theFormSet = theFormSetMaker(initial=[{'modelClass': IMAGE_SET_MODEL.get()}])
     return render_to_response("xgds_image/imageSearch.html", 
-                              {'templates': templates,
+                              {'imageSetsJson': "[]",
+                               'allAuthors': [{'author_name_index': [str(user.username), int(user.id)]} for user in User.objects.all()],
+                               'allCameras': [{'camera_name_index': [str(camera.name), int(camera.id)]} for camera in CAMERA_MODEL.get().objects.all()],
+                               'templates': templates,
                                'formset': theFormSet,
                                'app': 'xgds_map_server/js/simpleMapApp.js'},
                               context_instance=RequestContext(request))
