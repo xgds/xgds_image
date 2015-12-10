a29ff48138bda985c3c9111aad2b966557565677
Tamar Cohen <tamar.e.cohen@nasa.gov>
2015-12-10 13:08:38 -0800

    updated authors

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'xgds_image'
 copyright = u'2015, xGDS (Tamar Cohen)'
-author = u'xGDS (Tamar Cohen)'
+author = u'xGDS (Tamar Cohen, Yeon-Jin Grace Lee)'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
