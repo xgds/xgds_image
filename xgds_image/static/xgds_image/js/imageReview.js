a7889afc8fb129fefd18f66ffb98373bb41e360f
Grace Lee <yeongrace.lee@gmail.com>
2016-04-12 18:15:18 +0000

    image viewer debugged

--- xgds_image/static/xgds_image/js/imageReview.js
+++ xgds_image/static/xgds_image/js/imageReview.js
@@ -15,33 +15,71 @@
 //__END_LICENSE__
 
 var $container = $('#container'); 
-var inputNum = 0;
 
-function stringContains(string, substring) {
-	// checks that string contains substring
-	return string.indexOf(substring) > -1;
-}
 
 /**
- *  Selectable row in image table  
+ * Event binders
  */
-function handleImageSelection(theRow){
-    var newindex = $(theRow).index();
-    var $template = $(document.getElementById('image_view'));
-    if ($template) {
-    	updateImageView($template, newindex, null, false, false);
-    } else {
-    	constructImageView(imageSetsArray[newindex]);
-    }
+// Hook up edit info and add note buttons
+function activateButtons(template) {
+	template.find("#edit_info_button").click(function(event) {
+	    event.preventDefault();
+	    template.find("#image_overview").hide();
+	    template.find("#more_info_view").show();
+	});
+	template.find("#cancel_edit").click(function(event) {
+	    event.preventDefault();
+	    template.find("#more_info_view").hide();
+	    template.find("#image_overview").show();
+	});
+	template.find("#add_note_button").click(function(event) {
+	    event.preventDefault();
+	    template.find("#notes_input").show();
+	});
 }
 
 
-function setupTableSelection() {
-    connectSelectionCallback($("#image_table"), handleImageSelection, true);
+function setChangedPosition(value, template) {
+	$(template).find('#id_changed_position').attr('value', value);
 }
 
 
-/* Add a click handler for the delete row */
+// Set the changed_position to true if the user edits any of the position fields.
+function hookEditingPosition(template) {
+	$(template).find("#id_latitude").change(function() {setChangedPosition(1, template)});
+	$(template).find("#id_longitude").change(function() {setChangedPosition(1, template)});
+	$(template).find("#id_altitude").change(function() {setChangedPosition(1, template)});
+	$(template).find("#id_heading").change(function() {setChangedPosition(1, template)});
+}
+
+
+// Saves image info to the db when user updates it and submits.
+function onUpdateImageInfo(template) {
+	$(template).find("#updateInfoSubmit").on('click', function(event) {
+		event.preventDefault(); 	// avoid to execute the actual submit of the form.
+		var url = updateImageUrl; // the script where you handle the form input.
+		var postData = $("#more-info-form").serialize();
+		$.ajax({
+			url: url,
+			type: "POST",
+			data: postData, // serializes the form's elements.
+			success: function(data)
+			{
+			    setSaveStatusMessage($('#message'), 'Saved','');
+			    updateImageView(template, undefined, data[0], true, true);
+			    template.find("#more_info_view").hide();
+			    template.find("#image_overview").show();
+			},
+			error: function(request, status, error) {
+			    setSaveStatusMessage($('#message'), status, error);
+			}
+		});
+		return false; 
+	});
+}
+
+
+// Add a click handler for the delete row 
 $('#delete_images').click( function() {
     var selectedRows = getSelectedRows( theDataTable );
     // get a list containing just the id's of selected images
@@ -70,81 +108,12 @@ $('#delete_images').click( function() {
 } );
 
 
-/*
- * Event binders
- */
-/**
- * Hook up edit info and add note buttons
- */
-function activateButtons(template) {
-	template.find("#edit_info_button").click(function(event) {
-	    event.preventDefault();
-	    template.find("#image_overview").hide();
-	    template.find("#more_info_view").show();
-	});
-	template.find("#cancel_edit").click(function(event) {
-	    event.preventDefault();
-	    template.find("#more_info_view").hide();
-	    template.find("#image_overview").show();
-	});
-	template.find("#add_note_button").click(function(event) {
-	    event.preventDefault();
-	    template.find("#notes_input").show();
-	});
-}
 
-
-function setChangedPosition(value, template) {
-	$(template).find('#id_changed_position').attr('value', value);
-}
-
-
-/**
- * Set the changed_position to true if the user edits any of the position fields.
- * @param template
- */
-function hookEditingPosition(template) {
-	$(template).find("#id_latitude").change(function() {setChangedPosition(1, template)});
-	$(template).find("#id_longitude").change(function() {setChangedPosition(1, template)});
-	$(template).find("#id_altitude").change(function() {setChangedPosition(1, template)});
-	$(template).find("#id_heading").change(function() {setChangedPosition(1, template)});
-}
-
-/**
- * Saves image info to the db when user updates it and submits.
- */
-function onUpdateImageInfo(template) {
-	$(template).find("#updateInfoSubmit").on('click', function(event) {
-		event.preventDefault(); 	// avoid to execute the actual submit of the form.
-		var url = updateImageUrl; // the script where you handle the form input.
-		var postData = $("#more-info-form").serialize();
-		$.ajax({
-			url: url,
-			type: "POST",
-			data: postData, // serializes the form's elements.
-			success: function(data)
-			{
-			    setSaveStatusMessage($('#message'), 'Saved','');
-			    updateImageView(template, undefined, data[0], true, true);
-			    template.find("#more_info_view").hide();
-			    template.find("#image_overview").show();
-			},
-			error: function(request, status, error) {
-			    setSaveStatusMessage($('#message'), status, error);
-			}
-		});
-		return false; 
-	});
-}
-
-
-/* 
+/** 
  * Image next and previous button support
  */
-/**
- * Helper that finds index in imageSetsArray
- * of the currently displayed image in the imageView.
- */
+// Helper that finds index in imageSetsArray
+// of the currently displayed image in the imageView.
 function getCurrentImageAndIndex(template) {
 	var currentImageName = template.attr('id');
 	var currentImageIndex = null;
@@ -156,6 +125,7 @@ function getCurrentImageAndIndex(template) {
 	}
 	return currentImageIndex;
 }
+
 
 /**
  * Update the image view with newly selected image.
@@ -197,16 +167,15 @@ function updateImageView(template, index, imageJson, keepingImage, keepingNotes)
 	template.find('input[name="altitude"]').attr('value', imageJson['altitude']);
 	template.find('input[name="heading"]').attr('value', imageJson['heading']);
 	template.find('#id_changed_position').attr('value', 0);
-
-	// update table selection
-	ensureSelectedRow(theDataTable, index + 1);
 }
+
 
 function hideImageNextPrev() {
    $('.prev-button').hide();
    $('.next-button').hide();
    $('#new-window-btn').hide();
 }
+
 
 function onImageNextOrPrev(template) {
 	template.find('.next-button').click(function(event) {
@@ -234,100 +203,104 @@ function onImageNextOrPrev(template) {
 	});
 }
 
-/*
+
+/**
  * Construct the image view item
  */
 function constructImageView(json, viewPage) {
-	viewPage = typeof viewPage !== 'undefined' ? viewPage : false;
-	var rawTemplate = $('#template-image-view').html();
-	var compiledTemplate = Handlebars.compile(rawTemplate);
+	var imageViewWidget = $(".image_view_outer");
+	if (imageViewWidget.length) {
+		updateImageView(imageViewWidget, null, json, false, false);
+	} else {	
+		viewPage = typeof viewPage !== 'undefined' ? viewPage : false;
+		var rawTemplate = $('#template-image-view').html();
+		var compiledTemplate = Handlebars.compile(rawTemplate);
 
-	// append additional fields to json object to pass to handlebar
-	json.imageName = json['name'];
-	json.imagePath = json['raw_image_url'];
-	json.imageUrl = json['view_url'];
-	json.STATIC_URL = STATIC_URL;
-	json.acquisition_time = getLocalTimeString(json['acquisition_time'], json['acquisition_timezone']);
+		// append additional fields to json object to pass to handlebar
+		json.imageName = json['name'];
+		json.imagePath = json['raw_image_url'];
+		json.imageUrl = json['view_url'];
+		json.STATIC_URL = STATIC_URL;
+		json.acquisition_time = getLocalTimeString(json['acquisition_time'], json['acquisition_timezone']);
 
-	var newDiv = compiledTemplate(json);
-	var imageViewTemplate = $(newDiv);
+		var newDiv = compiledTemplate(json);
+		var imageViewTemplate = $(newDiv);
 
-	// callbacks
-	hookEditingPosition(imageViewTemplate);
-	onUpdateImageInfo(imageViewTemplate);
-	activateButtons(imageViewTemplate);
+		// callbacks
+		hookEditingPosition(imageViewTemplate);
+		onUpdateImageInfo(imageViewTemplate);
+		activateButtons(imageViewTemplate);
 
-	if (!viewPage){
-		bindDeleteButtonCallback(imageViewTemplate);
-		onImageNextOrPrev(imageViewTemplate);
-	}
-
-	// append the div to the container and gridstack.
-	var newEl;
-	if (!viewPage){
-		newEl = $container.append(imageViewTemplate);
-	} else {
-		newEl = $container.prepend(imageViewTemplate);
-	}
-	// add the element to the dashboard
-	if (!viewPage){
-		addItem(imageViewTemplate, 3, 3, 3, 2);
-	}
-
-//	// set the loading image to be displayed when main img is loading
-//	imageViewTemplate.find(".display-image").load(function() {
-//	// set dimensions of loading image
-//	var width = imageViewTemplate.find(".display-image").width();
-//	var height = imageViewTemplate.find(".display-image").height();
-//	imageViewTemplate.find(".loading-image").width(width);	
-//	imageViewTemplate.find(".loading-image").height(height);
-//	imageViewTemplate.find(".loading-image").hide();
-//	});
-//	setChangedPosition(0, imageViewTemplate);
-
-	//add the notes if it does not exist
-	var notes_content_div = imageViewTemplate.find("#notes_content");
-	var notes_table = undefined;
-	if ($(notes_content_div).is(':empty')){
-		// the first time we want to fill it in
-		notes_table = $.find("table#notes_list");
-		var notes_input_div = $.find("#notes_input");
-
-		var new_input_div = $(notes_input_div).hide();
-		$(notes_content_div).append(new_input_div);
-
-		var new_table_div = $(notes_table);
-		$(notes_content_div).append(new_table_div);
-		$(new_table_div).removeAttr('hidden');
-		$(new_table_div).show();
-
-		var taginput = $(new_input_div).find('.taginput');
-		initializeInput(taginput);
-		hookNoteSubmit();
-
-	} else {
-		notes_table = imageViewTemplate.find("table#notes_list");
-	}
-
-	initializeNotesReference(imageViewTemplate, json['app_label'], json['model_type'], json['id'], json['creation_time']);
-	getNotesForObject(json['app_label'], json['model_type'], json['id'], 'notes_content', $(notes_table));
-
-	// set the gridstack image height
-	$('.image_view_outer').attr('data-gs-height', '4');
-
-	// build tile sources for openseadragon viewer
-	var imageUrl = json['raw_image_url'];
-
-	// image viewer
-	var prefixUrl = STATIC_URL + '/openseadragon/built-openseadragon/openseadragon/images/';
-	viewer = OpenSeadragon({
-		id: "display-image",
-		prefixUrl: prefixUrl,
-		tileSources: {
-			type: 'image',
-			url: imageUrl
+		if (!viewPage){
+			onImageNextOrPrev(imageViewTemplate);
 		}
-	});
+
+		// append the div to the container and gridstack.
+		var newEl;
+		if (!viewPage){
+			newEl = $container.append(imageViewTemplate);
+		} else {
+			newEl = $container.prepend(imageViewTemplate);
+		}
+		// add the element to the dashboard
+		if (!viewPage){
+			addItem(imageViewTemplate, 3, 3, 3, 2);
+		}
+		//	// set the loading image to be displayed when main img is loading
+		//	imageViewTemplate.find(".display-image").load(function() {
+		//	// set dimensions of loading image
+		//	var width = imageViewTemplate.find(".display-image").width();
+		//	var height = imageViewTemplate.find(".display-image").height();
+		//	imageViewTemplate.find(".loading-image").width(width);	
+		//	imageViewTemplate.find(".loading-image").height(height);
+		//	imageViewTemplate.find(".loading-image").hide();
+		//	});
+		//	setChangedPosition(0, imageViewTemplate);
+
+		//add the notes if it does not exist
+		var notes_content_div = imageViewTemplate.find("#notes_content");
+		var notes_table = undefined;
+		if ($(notes_content_div).is(':empty')){
+			// the first time we want to fill it in
+			notes_table = $.find("table#notes_list");
+			var notes_input_div = $.find("#notes_input");
+
+			var new_input_div = $(notes_input_div).hide();
+			$(notes_content_div).append(new_input_div);
+
+			var new_table_div = $(notes_table);
+			$(notes_content_div).append(new_table_div);
+			$(new_table_div).removeAttr('hidden');
+			$(new_table_div).show();
+
+			var taginput = $(new_input_div).find('.taginput');
+			initializeInput(taginput);
+			hookNoteSubmit();
+
+		} else {
+			notes_table = imageViewTemplate.find("table#notes_list");
+		}
+
+		initializeNotesReference(imageViewTemplate, json['app_label'], json['model_type'], json['id'], json['creation_time']);
+		getNotesForObject(json['app_label'], json['model_type'], json['id'], 'notes_content', $(notes_table));
+
+		// set the gridstack image height
+		$('.image_view_outer').attr('data-gs-height', '4');
+
+		// build tile sources for openseadragon viewer
+		var imageUrl = json['raw_image_url'];
+
+		// image viewer
+		var prefixUrl = STATIC_URL + '/openseadragon/built-openseadragon/openseadragon/images/';
+		viewer = OpenSeadragon({
+			id: "display-image",
+			prefixUrl: prefixUrl,
+			tileSources: {
+				type: 'image',
+				url: imageUrl
+			}
+		});
+	}
 }
 
 
