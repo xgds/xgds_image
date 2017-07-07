49b9e2edca3eca6aeec4dd62be06211e25abaad7
William Lee <masteratsoccer@gmail.com>
2017-07-07 16:55:41 -0700

    Moved all assignments/initializers into initialize(). Still have OSD import error

--- xgds_image/static/xgds_image/js/imageAnnotation_namespace.js
+++ xgds_image/static/xgds_image/js/imageAnnotation_namespace.js
@@ -7,61 +7,10 @@ $.extend(xgds_image_annotation, {
     },
 
     // initialize viewer
-    viewer: OpenSeadragon({
-        id: "openseadragon1",
-        prefixUrl: prefixUrl,
-        showNavigator: false,
-        gestureSettingsMouse: {
-            clickToZoom: false
-        },
-        clickToZoom: "false",
-        tileSources: [{
-            Image: {
-                //Need to change [tile_format="jpg", image_quality=0.85,]
-                //Plus any other changes in deepzoom.py we make
-                xmlns: "http://schemas.microsoft.com/deepzoom/2009",
-                Url: "../../../../data/images/spacePics/ISS044-E-1998_files/",
-                TileSize: "128",
-                Overlap: "2",
-                Format: "png",
-                ServerFormat: "Default",
-                Size: {
-                    Width: "4928",
-                    Height: "3280",
-                }
-            }
-        }],
-    }),
-
-
-//     var viewer = OpenSeadragon({
-//         id: "openseadragon1",
-//         prefixUrl: prefixUrl,
-//         showNavigator: false,
-//         gestureSettingsMouse: {
-//             clickToZoom: false
-//         },
-//         clickToZoom: "false",
-//         tileSources: [{
-//             Image: {
-//                 //Need to change [tile_format="jpg", image_quality=0.85,]
-//                 //Plus any other changes in deepzoom.py we make
-//                 xmlns: "http://schemas.microsoft.com/deepzoom/2009",
-//                 Url: "../../../../data/images/spacePics/ISS044-E-1998_files/",
-//                 TileSize: "128",
-//                 Overlap: "2",
-//                 Format: "png",
-//                 ServerFormat: "Default",
-//                 Size: {
-//                     Width: "4928",
-//                     Height: "3280",
-//             }
-//         }
-//     }],
-// });
+    viewer: "",
 
     //fabricjs-openseadragon annotation object
-    overlay: viewer.fabricjsOverlay(),
+    overlay: "",
     arrow: "",
     line: "",
     rectangle: "",
@@ -112,10 +61,40 @@ $.extend(xgds_image_annotation, {
     },
 
     initialize: function(imageJson, osdViewer) {
-        //imageJson.pk is the pk of the image you want to work with now.
+        //TODO from Tamar
+        // imageJson.pk is the pk of the image you want to work with now.
         // if you were already initialized before, clear stuff
         // set your pk to be imageJson.pk
-        this.imagePK = imageJson.pk;
+        // this.imagePK = imageJson.pk;
+
+        // initialize viewer
+        this.viewer = OpenSeadragon({
+            id: "openseadragon1",
+            prefixUrl: prefixUrl,
+            showNavigator: false,
+            gestureSettingsMouse: {
+                clickToZoom: false
+            },
+            clickToZoom: "false",
+            tileSources: [{
+                Image: {
+                    //Need to change [tile_format="jpg", image_quality=0.85,]
+                    //Plus any other changes in deepzoom.py we make
+                    xmlns: "http://schemas.microsoft.com/deepzoom/2009",
+                    Url: "../../../../data/images/spacePics/ISS044-E-1998_files/",
+                    TileSize: "128",
+                    Overlap: "2",
+                    Format: "png",
+                    ServerFormat: "Default",
+                    Size: {
+                        Width: "4928",
+                        Height: "3280",
+                    }
+                }
+            }],
+        });
+
+        overlay = viewer.fabricjsOverlay();
         var prefixUrl = '/static/openseadragon/built-openseadragon/openseadragon/images/';
 
         //color picker
@@ -729,192 +708,13 @@ $.extend(xgds_image_annotation, {
 
 
 
-
-//fabricJS mouse-down event listener
-overlay.fabricCanvas().observe('mouse:down', function (o) {
-    // console.log("EVENT TRIGERRED: fabricjs-mouse:down");
-    console.log("mouse mode is " + getMouseMode());
-    if (getMouseMode() == "addAnnotation") {
-        isDown = true;
-        var pointer = overlay.fabricCanvas().getPointer(o.e);
-        origX = pointer.x;
-        origY = pointer.y;
-        switch (annotationType) {
-            case "arrow":
-                drawArrow(pointer.x, pointer.y);
-                break;
-            case "rectangle":
-                initializeRectangle(pointer.x, pointer.y);
-                break;
-            case "ellipse":
-                initializeEllipse(pointer.x, pointer.y);
-                break;
-            case "text":
-                // initializeText(pointer.x, pointer.y);
-                initializeTextboxPreview(pointer.x, pointer.y);
-                break;
-            default:
-                console.log("welp, that shouldn't have happened. Undefined annotationType");
-                throw new Error("Tried to switch to an undefined annotationType");
-        }
-    }
-});
-
-//fabricJS mouse-move event listener
-overlay.fabricCanvas().observe('mouse:move', function (o) {
-    if (!isDown) return;
-    var pointer = overlay.fabricCanvas().getPointer(o.e);
-
-    switch (annotationType) {
-        case "arrow":
-            updateArrow(pointer.x, pointer.y);
-            break;
-        case "rectangle":
-            updateRectangleWidth(pointer.x, pointer.y);
-            break;
-        case "ellipse":
-            updateEllipse(pointer.x, pointer.y);
-            break;
-        case "text":
-            updateTextboxPreview(pointer.x, pointer.y);
-            break;
-        default:
-            console.log("welp, that shouldn't have happened. Undefined annotationType");
-            throw new Error("Tried to switch to an undefined annotationType");
-    }
-    overlay.fabricCanvas().renderAll();
-});
-
-/* event listener that handles resizing the textbox based on amount of text */
-overlay.fabricCanvas().on('text:changed', function (opt) {
-    var t1 = opt.target;
-    if (t1.width > t1.fixedWidth) {
-        t1.fontSize *= t1.fixedWidth / (t1.width + 1);
-        t1.width = t1.fixedWidth;
-    }
-});
-
-//fabricJS mouse-up event listener
-overlay.fabricCanvas().on('mouse:up', function (o) {
-    // console.log("EVENT TRIGERRED: fabricj-mouse:up");
-    if (getMouseMode() == "addAnnotation") {
-        var pointerOnMouseUp = overlay.fabricCanvas().getPointer(event.e);
-
-        // save annotation to database
-        createNewSerialization(currentAnnotationType, pointerOnMouseUp.x, pointerOnMouseUp.y);
-        setMouseMode("OSD");
-        $("#navigateImage").click(); // change nav bar back to OSD (navigateImage)
-    }
-    isDown = false;
-});
-
-$("input[name='cursorMode']").change(function () {
-    // console.log("cursorMode change detected: " + $("input[name='cursorMode']:checked").val());
-    var mode = $("input[name='cursorMode']:checked").val();
-    setMouseMode(mode);
-});
-
-$("input[name='annotationsOnOrOff']").change(function () {
-    var onOff = $("input[name='annotationsOnOrOff']:checked").val();
-    var objects = overlay.fabricCanvas().getObjects();
-    if (onOff == "off") {
-        for (var i = 0; i < objects.length; i++) {
-            //set all objects as invisible and lock in position
-            objects[i].visible = false;
-            objects[i].lockMovementX = true;
-            objects[i].lockMovementY = true;
-            objects[i].lockRotation = true;
-            objects[i].lockScalingFlip = true;
-            objects[i].lockScalingX = true;
-            objects[i].lockScalingY = true;
-            objects[i].lockSkewingX = true;
-            objects[i].lockSkewingY = true;
-            objects[i].lockUniScaling = true;
-        }
-    } else {
-        //set all objects as visible and unlock
-        for (var i = 0; i < objects.length; i++) {
-            objects[i].visible = true;
-            objects[i].lockMovementX = false;
-            objects[i].lockMovementY = false;
-            objects[i].lockRotation = false;
-            objects[i].lockScalingFlip = false;
-            objects[i].lockScalingX = false;
-            objects[i].lockScalingY = false;
-            objects[i].lockSkewingX = false;
-            objects[i].lockSkewingY = false;
-            objects[i].lockUniScaling = false;
-        }
-    }
-    overlay.fabricCanvas().renderAll();
-});
-
-$("input[name='annotationType']").change(function () {
-    // console.log("annotationType change detected: " + $("input[name='annotationShape']:checked").val());
-    annotationType = $("input[name='annotationType']:checked").val();
-    console.log("annotation shape changed to: " + annotationType);
-});
-
-overlay.fabricCanvas().on('object:modified', function () {
-    updateSerialization(overlay.fabricCanvas().getActiveObject());
-});
-
-$("#addAnnotation").click(function () {
-    setMouseMode("addAnnotation");
-});
-
-$('#loadAnnotation').click(function () {
-    getAnnotations();
-});
-
-$('#downloadScreenshot').click(function () {
-    var OSD_layer = viewer.drawer.canvas.toDataURL("image/png");
-    var annotations = overlay.fabricCanvas().toDataURL({format: 'image/png'});
-
-    $.ajax({
-        type: "POST",
-        url: '/xgds_image/mergeImages/',
-        datatype: 'json',
-        data: {
-            image1: OSD_layer,
-            image2: annotations
-        },
-        success: function (base64string) {  //do we have to index data?
-            console.log("IMAGE MERGE SUCCESS");
-            window.open("data:image/png;base64," + base64string);
-            // put in image tag and see if black bars/transparency still there
-
-        },
-        error: function (e) {
-            console.log("Ajax error");
-            console.log(e);
-        }
-    });
-});
-
-$('#deleteAnnotation').click(function () {
-    deleteActiveAnnotation();
-});
-
-$("#colorPicker").on('change.spectrum', function (e, color) {
-    currentAnnotationColor = color.toHexString(); //convert to hex
-    console.log("currentAnnotationColor is: " + currentAnnotationColor);
-});
+// TODO: I think the issue is you can't do assignment in a namespace field like that?
+// Need to declare as "" and initialize it in initialize()
 
 
+// Now, just need to figure out the OSD import error...
 
-function objectListToJsonList(list) {
-    var retval = [];
-    list.foreach(function (object) {
-        retval.push(duplicateObject(object));
-    });
-    return JSON.stringify(retval);
-}
-
-
-
-
-
+xgds_image_annotation.initialize();
 
 /****************************************************************************************************************
 
