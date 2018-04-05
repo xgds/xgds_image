8b178d13c24593d95a64f0b2c81faa339c169287
Tamar Cohen <tamar.cohen@nasa.gov>
2018-04-05 16:42:05 -0700

    default image for search inclusion

--- xgds_image/defaultSettings.py
+++ xgds_image/defaultSettings.py
@@ -53,25 +53,38 @@ XGDS_CORE_TEMPLATE_DIRS = getOrCreateDict('XGDS_CORE_TEMPLATE_DIRS')
 XGDS_CORE_TEMPLATE_DIRS[XGDS_IMAGE_IMAGE_SET_MODEL] = [os.path.join('xgds_image', 'templates', 'handlebars')]
 
 XGDS_MAP_SERVER_JS_MAP = getOrCreateDict('XGDS_MAP_SERVER_JS_MAP')
+STATIC_URL = '/static/'
+EXTERNAL_URL = STATIC_URL
 XGDS_MAP_SERVER_JS_MAP[XGDS_IMAGE_IMAGE_SET_MONIKER] = {'ol': 'xgds_image/js/olImageMap.js',
                                                         'model': XGDS_IMAGE_IMAGE_SET_MODEL,
-                                                        'columns': ['acquisition_time', 'timezone', 'name', 'author', 'thumbnail_image_url'],
-                                                        'columnTitles': ['Acquisition', 'Timezone', 'Name', 'Author', ''],
+                                                        'searchableColumns': ['name','description','flight_name', 'author_name'],
+                                                        'columns': ['checkbox', 'acquisition_time', 'acquisition_timezone', 'author_name', 'name', 'description', 'thumbnail_image_url',  'pk', 'view_url',
+                                                                    'camera_name', 'raw_image_url', 'app_label', 'model_type', 'type', 'lat', 'lon', 'alt', 'head','flight_name', 'deepzoom_file_url',
+                                                                    'rotation_degrees', 'originalImageResolutionString', 'originalImageFileSizeMB', 'create_deepzoom','DT_RowId'],
+                                                        'hiddenColumns': ['pk', 'view_url', 'camera_name', 'raw_image_url', 'app_label',  'resource_name', 'model_type','type',
+                                                                          'lat','lon','alt','head','flight_name', 'deepzoom_file_url', 'rotation_degrees',
+                                                                          'originalImageResolutionString', 'originalImageFileSizeMB', 'create_deepzoom', 'DT_RowId'],
+                                                        'unsortableColumns': ['thumbnail_image_url'],
+                                                        'columnTitles': ['Time', 'TZ', 'Author', 'Name',  'Description', 'Image'],
                                                         'viewHandlebars': 'xgds_image/templates/handlebars/image-view2.handlebars',
-                                                        'viewJS': ['/static/openseadragon/built-openseadragon/openseadragon/openseadragon.min.js',
-                                                                   '/static/xgds_image/js/imageAnnotation.js',
-                                                                   '/static/openseadragon/built-openseadragon/openseadragon/openseadragon.js',
-                                                                   '/static/fabric.js/dist/fabric.min.js',
-                                                                   '/static/openseadragon-fabricjs-overlay/openseadragon-fabricjs-overlay.js',
-                                                                   '/static/spectrum-colorpicker/spectrum.js',
-                                                                   '/static/xgds_image/js/imageReview.js'],
-
-
-                                                        'viewCss': ['/static/xgds_image/css/xgds_image.css'],
+                                                        'viewJS': [EXTERNAL_URL + 'openseadragon/built-openseadragon/openseadragon/openseadragon.min.js',
+                                                                   EXTERNAL_URL + 'openseadragon/built-openseadragon/openseadragon/openseadragon.js',
+                                                                   EXTERNAL_URL + 'fabric.js/dist/fabric.min.js',
+                                                                   EXTERNAL_URL + 'openseadragon-fabricjs-overlay/openseadragon-fabricjs-overlay.js',
+                                                                   EXTERNAL_URL + 'spectrum-colorpicker/spectrum.js',
+                                                                   EXTERNAL_URL + 'jquery-fileDownload/src/Scripts/jquery.fileDownload.js',
+                                                                   STATIC_URL + 'xgds_image/js/imageAnnotation.js',
+                                                                   STATIC_URL + 'xgds_image/js/imageReview.js' ],
+                                                        'viewCss': [STATIC_URL + 'xgds_image/css/xgds_image.css',
+                                                                    EXTERNAL_URL + 'spectrum/spectrum.css'],
                                                         'viewInitMethods': ['xgds_image.setupImageViewer'],
+                                                        'viewResizeMethod': ['xgds_image.resizeImageViewer'],
                                                         'event_time_field': 'acquisition_time',
-                                                        'event_timezone_field': 'acquisition_timezone'
-                                                        } 
+                                                        'event_timezone_field': 'acquisition_timezone',
+                                                        'saveRotationUrl': '/xgds_image/saveRotation/',
+                                                        'getRotationUrl': '/xgds_image/getRotation/',
+                                                        'search_form_class': 'xgds_image.forms.SearchImageSetForm'
+                                                       }
 
 XGDS_DATA_IMPORTS = getOrCreateDict('XGDS_DATA_IMPORTS')
 XGDS_DATA_IMPORTS[XGDS_IMAGE_IMAGE_SET_MONIKER + 's'] = '/xgds_image/import'
@@ -83,3 +96,4 @@ XGDS_IMAGE_ARROW_ANNOTATION_MODEL = 'xgds_image.ArrowAnnotation'
 XGDS_IMAGE_ELLIPSE_ANNOTATION_MODEL = 'xgds_image.EllipseAnnotation'
 XGDS_IMAGE_RECTANGLE_ANNOTATION_MODEL = 'xgds_image.RectangleAnnotation'
 XGDS_IMAGE_TEXT_ANNOTATION_MODEL = 'xgds_image.TextAnnotation'
+
